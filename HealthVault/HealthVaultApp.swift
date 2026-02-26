f2055183113a64566c6c6040033beed98122a5d2
zahirulAIIUB <jislam150@gmail.com>
2026-02-26 07:55:36 +0600

    Initial Commit

---
+++ HealthVault/HealthVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthVaultApp.swift
+//  HealthVault
+//
+//  Created by lynkto_1 on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthVaultTests/HealthVaultTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthVaultTests.swift
+//  HealthVaultTests
+//
+//  Created by lynkto_1 on 2/26/26.
+//
+
+import Testing
+@testable import HealthVault
+
+struct HealthVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
